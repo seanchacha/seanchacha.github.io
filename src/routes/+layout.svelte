bd1440cdf5c48717f0d2c97b87742370dc50e4ba
seanc-hub <sean.cha@venhub.com>
2026-01-01 02:18:28 -0800

    move darkmode button to right

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,15 +25,17 @@
 
 	<MyNavbar />
 
-	<Button onclick={toggleMode} variant="outline" size="icon">
-	<SunIcon
-		class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
-	/>
-	<MoonIcon
-		class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
-	/>
-	<span class="sr-only">Toggle theme</span>
-	</Button>
+	<div class="ml-auto pl-3 pr-4">
+		<Button onclick={toggleMode} variant="outline" size="icon">
+		<SunIcon
+			class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+		/>
+		<MoonIcon
+			class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+		/>
+		<span class="sr-only">Toggle theme</span>
+		</Button>
+	</div>
 </div>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
