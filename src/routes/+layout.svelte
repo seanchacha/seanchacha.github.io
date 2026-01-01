7e949308ac5cbde39334c62fed1797938117a831
seanc-hub <sean.cha@venhub.com>
2026-01-01 01:44:57 -0800

    navbar update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,13 +10,17 @@
 
 <ModeWatcher />
 
-<!-- favicon preview (decorative) -->
-<div class="favicon-container" aria-hidden="true">
-  <img src={favicon} alt="favicon preview" class="favicon-image" />
+<div class="flex items-center px-4 py-2">
+	<!-- favicon preview (decorative) -->
+	<div class="favicon-container pl-2 pr-4" aria-hidden="true">
+	<img 
+		src={favicon}
+		alt="favicon"
+		class="h-8 w-8 object-contain" />
+	</div>
+
+	<MyNavbar />
 </div>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-
-<MyNavbar />
-
 {@render children()}
