f5f1eac23014d0ea3e8d3aa327028b2deea86259
seanc-hub <sean.cha@venhub.com>
2026-01-01 02:22:49 -0800

    change main page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,6 @@
 </script>
 
 
-<div class="flex flex-col items-center justify-center min-h-screen">
-	<h1>UNDER CONSTRUCTION! MIGRATING TO SVELTE SITE, IN PROGRESS!</h1>
+<div class="flex flex-col items-center justify-center min-h-screen px-12">
+	<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">UNDER CONSTRUCTION! MIGRATING TO SVELTE SITE, IN PROGRESS!</h3>
 </div>
