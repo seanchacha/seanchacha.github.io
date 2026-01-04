3ba02da92ed04a8fe21fdfa56a5306dfefa9b37a
seanc-hub <sean.cha@venhub.com>
2026-01-03 21:49:50 -0800

    added resume link

--- src/lib/components/MyInfo.svelte
+++ src/lib/components/MyInfo.svelte
@@ -8,29 +8,32 @@
   const items = [
     { label: 'Phone', text: '(213) 505-0769' , href: null},
     { label: 'Email', text: 'seungwcha@gmail.com', href: null },
-    { label: 'Resume', text: 'Click here', href: null }
+    { label: 'Resume', text: 'Click here', href: "https://drive.google.com/file/d/10YZICmNcw1uLvkIORbsS7znNG63yWNEf/view?usp=sharing" }
   ];
 </script>
 
 <div class="my-6 w-full justify-center {isMobile.current ? 'flex flex-col gap-2 max-w-80 mx-auto' : 'flex flex-wrap gap-2'}">
-{#each items as item}
-    {#if item.href}
-        <a 
-        href={item.href} 
-        class="text-center {isMobile.current ? 'w-full' : 'flex-1 min-w-[200px] max-w-10'}"
-        >
-        {item.label}
-        <br />
-        {item.text}
-        </a>
-    {:else}
-        <div 
-        class="text-center {isMobile.current ? 'w-full' : 'flex-1 min-w-[200px] max-w-10'}"
-        >
-        {item.label}
-        <br />
-        {item.text}
-        </div>
-    {/if}
-{/each}
+  {#each items as item}
+      {#if item.href}
+          <a 
+          href={item.href} 
+          class="text-center {isMobile.current ? 'w-full' : 'flex-1 min-w-[200px] max-w-10'}"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Sean resume"
+          >
+          {item.label}
+          <br />
+          <p class="underline">{item.text}</p>
+          </a>
+      {:else}
+          <div 
+          class="text-center {isMobile.current ? 'w-full' : 'flex-1 min-w-[200px] max-w-10'}"
+          >
+          {item.label}
+          <br />
+          {item.text}
+          </div>
+      {/if}
+  {/each}
 </div>
