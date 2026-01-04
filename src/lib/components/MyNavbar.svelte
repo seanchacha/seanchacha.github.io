4a4183eb949c254b2bbcb939cb90959489b4fa9d
seanc-hub <sean.cha@venhub.com>
2026-01-03 16:56:11 -0800

    remove projects page

--- src/lib/components/MyNavbar.svelte
+++ src/lib/components/MyNavbar.svelte
@@ -12,21 +12,9 @@
   <NavigationMenu.Item>
    <NavigationMenu.Link>
     {#snippet child()}
-     <a href="/projects" class={navigationMenuTriggerStyle()}>Projects</a>
+     <a href="/blogs" class={navigationMenuTriggerStyle()}>Blogs</a>
     {/snippet}
    </NavigationMenu.Link>
-  </NavigationMenu.Item>
-  <NavigationMenu.Item>
-   <NavigationMenu.Trigger>Blogs</NavigationMenu.Trigger>
-   <NavigationMenu.Content>
-    <ul class="grid w-[200px] gap-4 p-2">
-     <li>
-      <NavigationMenu.Link href="/blogs/">ALL POSTS</NavigationMenu.Link>
-      <NavigationMenu.Link href="/blogs/this-site">Build + Host this Site for Free</NavigationMenu.Link>
-      <NavigationMenu.Link href="/blogs/life">Placeholder(?)</NavigationMenu.Link>
-     </li>
-    </ul>
-   </NavigationMenu.Content>
   </NavigationMenu.Item>
  </NavigationMenu.List>
 </NavigationMenu.Root>
